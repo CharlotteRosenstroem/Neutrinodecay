f5e19691ee3e7f0985a1c05b5df53081c45180ee
CharlotteRosenstroem <charlotte_rosenstroem@hotmail.com>
2017-05-03 14:27:31 +0200

    Add files via upload

---
+++ neutrinomassratio.m
@@ -0,0 +1,7 @@
+theta12=0.5764;
+theta13=0.1463; 
+theta23=0.7223;
+
+v1=cos(theta13)*cos(theta12)-2*cos(theta23)*sin(theta12)-2*sin(theta23)*cos(theta12)*sin(theta13)
+v2=cos(theta13)*sin(theta12)+2*cos(theta23)*cos(theta12)-2*sin(theta23)*sin(theta12)*sin(theta13)
+v3=sin(theta13)+2*cos(theta13)*sin(theta23)
